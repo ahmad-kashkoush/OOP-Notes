165a2520106d5224b545be3860bf8b2984574587
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-03-03 19:03:59 +0200

    Adding Clone function which return new copy of pointer to work with

--- Polymorphism/#291_293 Polymorphism.cpp
+++ Polymorphism/#291_293 Polymorphism.cpp
@@ -6,32 +6,53 @@ using namespace std;
 class Shape{
 public:
     virtual void PrintArea(){cout<<"Shape";}
+    virtual Shape* Clone(){return nullptr;}
 };
 class Circle: public Shape{
 private:
    double radius;
 public:
     Circle(double a):radius(a){}
-    void PrintArea(){
+    virtual void PrintArea()override{
         cout<<"Circle Area: "<<(double)radius*radius*(double(22/7))<<el;
     }
+    Circle()=default;
+    virtual Shape *Clone()override{return new Circle(*this);}
 };
 class Rectangle:public Shape{
+private:
     int w, h;
 public:
+    Rectangle()=default;
     Rectangle(int a, int b):w(a),h(b){}
-    void PrintArea(){
+    virtual void PrintArea()override{
         cout<<"Rectangle Area: "<<w*h<<el;
+    }
+    virtual Shape *Clone()override{
+        return new Rectangle(*this);
+    }
+};
+class Editor{
+private:
+    vector<Shape*> Shapes;
+public:
+    Editor(){Shapes.clear();}
+    void AddShape(Shape *sh){
+        Shapes.emplace_back(sh->Clone());
+    }
+    void Print(){
+        for(auto i:Shapes)
+            i->PrintArea();
+    }
+    ~Editor(){
+        for(auto ShapePtr:Shapes)
+            delete ShapePtr;
     }
 };
 int main(){
-        //[circle, rectangle, rectangle, circle]
-        vector<Shape *> v(4);
-        v[0]= new Circle(7.0);
-        v[1]= new Rectangle(2, 4);
-        v[2]= new Rectangle(4, 8);
-        v[3]= new Circle(14);
-        for(auto i:v)
-            i->PrintArea();
-
+    Editor editor;
+    editor.AddShape(new Circle(7));
+    editor.AddShape(new Rectangle(2, 4));
+    editor.AddShape(new Circle(14));
+    editor.Print();
 }
